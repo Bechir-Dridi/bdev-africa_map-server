14a590007646733786db3dc4a44142d697db52ab
Bechir-Dridi <bechir7dridi#gmail.com>
2024-05-24 01:05:36 +0100

    cors config changed

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ require("dotenv").config()
 // Middleware to enable CORS
 app.use(cors(
     //{ origin: "*", credentials: true, }, //server accepts requests from static site
-    { origin: ["http://localhost:3000", "https://bdev-saitama.netlify.app"], credentials: true, } //server accepts requests from static site
+    { origin: "https://bdev-africamap.vercel.app", credentials: true, } //server accepts requests from static site
 ))
 app.use(express.json())//body parsing middleware for body
 app.use(express.urlencoded({ extended: true }));//body parsing middleware for formUrlEncoded
